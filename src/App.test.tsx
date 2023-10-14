dc9505fec031255d52c2dc14020d95496f9fa254
romomartin <romoander@gmail.com>
2023-10-14 19:46:34 +0200

    fix app test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,10 +5,10 @@ describe("app", () => {
   it("renders the map on initial state", () => {
     render(<App />);
     const style = screen.getByText(/mapbox:\/\/styles\/mapbox\/outdoors-v12/i);
-    const initialLatitude = screen.getByText(/"latitude": 43.21861/i);
-    const initialLongitude = screen.getByText(/"longitude": -2.94305/i);
-    const initialZoom = screen.getByText(/"zoom": 13/i);
-    const interactiveLayers = screen.getByText(/"interactiveLayerIds"tracks/i);
+    const initialLatitude = screen.getByText(/lat: 43.21861/i);
+    const initialLongitude = screen.getByText(/long: -2.94305/i);
+    const initialZoom = screen.getByText(/zoom: 13/i);
+    const interactiveLayers = screen.getByText(/interactiveLayerIds:.*tracks/i);
 
     expect(style).toBeInTheDocument();
     expect(initialLatitude).toBeInTheDocument();
