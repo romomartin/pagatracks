44bd3e8e022ae0d9432d425f540a091992ffba6e
romomartin <romoander@gmail.com>
2023-10-25 20:41:12 +0200

    Calculate elevation data from track geometry

--- src/elevation-chart/ElevationChart.tsx
+++ src/elevation-chart/ElevationChart.tsx
@@ -3,6 +3,9 @@ import { FunctionComponent } from "react";
 import HighchartsReact from "highcharts-react-official";
 import Highcharts from "highcharts";
 import { Track } from "../tracks/track";
+import { MultiLineString, Position } from "geojson";
+import { getDistance } from "geolib";
+import { GeolibInputCoordinates } from "geolib/es/types";
 
 type Props = {
   selectedTrack: Track | undefined;
@@ -11,16 +14,13 @@ type Props = {
 export const ElevationChart: FunctionComponent<Props> = ({
   selectedTrack,
 }: Props) => {
+  const trackGeometry = selectedTrack?.geometry as MultiLineString;
   const chartOptions: Highcharts.Options = {
     title: { text: selectedTrack?.properties.name },
     series: [
       {
         type: "line",
-        data: [
-          [1, 1],
-          [2, 1],
-          [3, 2],
-        ],
+        data: elevationDataFrom(trackGeometry),
       },
     ],
     chart: {
@@ -39,4 +39,28 @@ export const ElevationChart: FunctionComponent<Props> = ({
       ></HighchartsReact>
     </div>
   );
+};
+
+const elevationDataFrom = (geometry: MultiLineString): number[][] => {
+  const firstPosition = geometry.coordinates[0][0];
+  let previousPosition = firstPosition;
+  let distance = 0;
+  const elevationData = geometry.coordinates[0].map((position) => {
+    distance += getDistance(
+      positionToGeolibInputCoordinates(previousPosition),
+      positionToGeolibInputCoordinates(position)
+    );
+    previousPosition = position;
+    return [distance, position[2]];
+  });
+  return elevationData;
+};
+
+const positionToGeolibInputCoordinates = (
+  position: Position
+): GeolibInputCoordinates => {
+  return {
+    longitude: position[0],
+    latitude: position[1],
+  } as GeolibInputCoordinates;
 };
