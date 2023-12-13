d94b96130dd06dd4cabe42dafcb1b436282377b5
romomartin <romoander@gmail.com>
2023-12-13 20:00:26 +0100

    Update merge-raw-tracks helper script to use path module

--- data-utils/merge-raw-tracks.js
+++ data-utils/merge-raw-tracks.js
@@ -1,21 +1,25 @@
 const fs = require('fs');
+const path = require('path')
 
 const DATA_URL = '../dummy-data' 
 const DESTINATION_URL = '../public/data/mergedRawTracks.json'
 
+const dataPath = path.resolve(__dirname, DATA_URL);
+const destinationPath = path.resolve(__dirname, DESTINATION_URL);
+
 const mergeRawTracks = () => {
     const rawTracks = [];
   
-    fs.readdirSync(DATA_URL).forEach((fileName) => {
+    fs.readdirSync(dataPath).forEach((fileName) => {
       if (!fileName.includes("geojson")) return;
   
-      const rawTrack = JSON.parse(fs.readFileSync(`${DATA_URL}/${fileName}`, "utf8"));
+      const rawTrack = JSON.parse(fs.readFileSync(`${dataPath}/${fileName}`, "utf8"));
       rawTracks.push(rawTrack);
     });
 
     const mergedRawTracks = JSON.stringify(rawTracks);
   
-    fs.writeFileSync(`${DESTINATION_URL}`,mergedRawTracks )
+    fs.writeFileSync(`${destinationPath}`,mergedRawTracks )
     console.log(`✅ ${rawTracks.length} tracks merged in ${DESTINATION_URL}`)
 };
 
