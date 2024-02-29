ef26cd5b231daa7c3f41cd9d488d28f6a1feee87
romomartin <romoander@gmail.com>
2024-02-29 20:27:12 +0100

    Update tracks merging util

--- data-utils/merge-raw-tracks.js
+++ data-utils/merge-raw-tracks.js
@@ -1,26 +1,19 @@
 const fs = require('fs');
 const path = require('path')
 
-const DATA_URL = '../dummy-data' 
-const DESTINATION_URL = '../public/data/mergedRawTracks.json'
+const DATA_URL = '../dummy-data/TRACKS.geojson' 
+const DESTINATION_URL = '../public/data/tracks.json'
 
 const dataPath = path.resolve(__dirname, DATA_URL);
 const destinationPath = path.resolve(__dirname, DESTINATION_URL);
 
 const mergeRawTracks = () => {
-    const rawTracks = [];
-  
-    fs.readdirSync(dataPath).forEach((fileName) => {
-      if (!fileName.includes("geojson")) return;
-  
-      const rawTrack = JSON.parse(fs.readFileSync(`${dataPath}/${fileName}`, "utf8"));
-      rawTracks.push(rawTrack);
-    });
+  const rawTracks = JSON.parse(fs.readFileSync(`${dataPath}`, "utf8"));
 
-    const mergedRawTracks = JSON.stringify(rawTracks);
+  const mergedRawTracks = JSON.stringify(rawTracks);
   
-    fs.writeFileSync(`${destinationPath}`,mergedRawTracks )
-    console.log(`✅ ${rawTracks.length} tracks merged in ${DESTINATION_URL}`)
+  fs.writeFileSync(`${destinationPath}`,mergedRawTracks )
+  console.log(`✅ tracks merged in ${DESTINATION_URL}`)
 };
 
 mergeRawTracks()
